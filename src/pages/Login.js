19e00ab9d7ed4473c004eb99c02e666331b3738a
dongdong <dongdong@eyesvision.com>
2021-07-08 14:39:50 +0900

    디자인

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,64 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { signIn, signInWithGoogle } from "../helpers/auth";
 
+
+
+
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+
+
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Button component={Link} to="https://material-ui.com/">
+        Your Website
+      </Button>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 function Login() {
+  const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -36,47 +93,84 @@ function Login() {
   };
 
 
-  return (
-    <div className="sign-container">
-      <div className="sign-wrap">
-        <h1 className="title">로그인</h1>
-        <form className="sign-form" onSubmit={handleOnSubmit}>
-          <div>
-            <input
-              type="email"
-              placeholder="이메일을 입력하세요."
-              name="email"
-              value={email}
-              onChange={handleOnChange}
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              placeholder="비밀번호를 입력하세요."
-              name="password"
-              value={password}
-              onChange={handleOnChange}
-            />
-          </div>
-          <div>
-            <button type="submit">로그인</button>
-          </div>
+  return (    
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          로그인
+        </Typography>
+        <form className={classes.form} onSubmit={handleOnSubmit} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="이메일을 입력하세요."
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={handleOnChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={handleOnChange}
+          />
+          {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={handleGoogleSignIn}
+            className={classes.submit}
+          >
+            구글 로그인
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              {/* <Link href="#" variant="body2">
+                Forgot password?
+              </Link> */}
+            </Grid>
+            <Grid item>
+              <Button component={Link} to="/signup">
+                {"회원가입"}
+              </Button>
+            </Grid>
+          </Grid>
         </form>
-        <hr></hr>
-        <button
-          type="button"
-          className="sign-social-btn google-login"
-          onClick={handleGoogleSignIn}
-        >
-          구글 로그인
-        </button>
-        <hr></hr>
-        <p>
-          회원이 아니신가? <Link to="/signup">회원가입</Link>
-        </p>
       </div>
-    </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,7 +2,61 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { signUp } from "../helpers/auth";
 
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Button component={Link} to="https://material-ui.com/">
+        Your Website
+      </Button>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+
+
 function SignUp() {
+  const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -28,38 +82,94 @@ function SignUp() {
   };
 
   return (
-    <div className="sign-container">
-      <div className="sign-up-wrap">
-        <h1 className="title">회원가입</h1>
-        <form className="sign-up-form" onSubmit={handleOnSubmit}>
-          <div>
-            <input
-              type="email"
-              placeholder="이메일을 입력하세요."
-              name="email"
-              value={email}
-              onChange={handleOnChange}
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              placeholder="비밀번호를 입력하세요."
-              name="password"
-              value={password}
-              onChange={handleOnChange}
-            />
-          </div>
-          <div>
-            <button type="submit">회원가입</button>
-          </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          회원 가입
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleOnSubmit}>
+          <Grid container spacing={2}>
+            {/* <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid> */}
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={handleOnChange}
+              />
+            </Grid>
+            {/* <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid> */}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            가입하기
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              이미 회원이신가? <Button component={Link} to="/login">로그인</Button>
+            </Grid>
+          </Grid>
         </form>
-        <hr></hr>
-        <p>
-          이미 회원이신가? <Link to="/login">로그인</Link>
-        </p>
       </div>
-    </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
